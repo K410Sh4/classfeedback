5cb16fd39762a46b45664d617cd078f05a66c24e
Zyphon <elgator833@gmail.com>
2026-09-18 18:45:59 -0300

    Add multi-node board configuration

---
+++ private_link_firmware/src/BoardConfig.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Arduino.h>
+
+#if CONFIG_IDF_TARGET_ESP32S3
+
+#define PL_FW_VERSION "1.4.0"
+#define PL_MODEL "ESP32-S3-N16R8"
+#define PL_BOARD "PrivateLink-S3"
+#define PL_ROLE "PRIMARY_NODE"
+#define PL_CAPS "BLE,WIFI,RESEARCH,FAST_OTA"
+
+#elif CONFIG_IDF_TARGET_ESP32C6
+
+#define PL_FW_VERSION "1.0.0-c6"
+#define PL_MODEL "nanoESP32-C6-V1711"
+#define PL_BOARD "nanoESP32-C6-V1.0"
+#define PL_ROLE "RADIO_NODE"
+#define PL_CAPS "BLE,WIFI,RESEARCH,FAST_OTA,IEEE802154_READY"
+
+#else
+#error "Unsupported target for PrivateLink Multi-Node firmware"
+#endif
